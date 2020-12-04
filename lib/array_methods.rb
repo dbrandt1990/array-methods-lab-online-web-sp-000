3ecc85e5c00c13cc2214eb0ae52d5561e1983240
Danny Brandt <dvbrandt90@gmail.com>
2020-12-04 18:35:32 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,15 +3,15 @@ array.include?(element)
 end
 
 def using_sort(array)
-array.sort()
+array.sort
 end
 
 def using_reverse(array)
-array.reverse()
+array.reverse
 end
 
 def using_first(array)
-array.first()
+array.first
 end
 
 def using_last(array)
